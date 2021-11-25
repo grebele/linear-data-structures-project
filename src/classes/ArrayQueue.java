81652aaa6396391aefee8341e36668b707baa24b
Koller Simon Domingo Joao <simondomingojoao.koller@students.bfh.ch>
2021-11-25 09:47:46 +0000

    Update ArrayQueue.java

--- src/classes/ArrayQueue.java
+++ src/classes/ArrayQueue.java
@@ -1,7 +1,6 @@
 package classes;
 
 import exceptions.EmptyQueueException;
-import exceptions.FullQueueException;
 import interfaces.Queue;
 
 public class ArrayQueue<E> implements Queue<E> {
@@ -9,50 +8,92 @@ public class ArrayQueue<E> implements Queue<E> {
 	private int frontIndex;
 	private int size;
 	private int capacity;
-	private E elements[];
+	private Object[] arr;
 	
+	public ArrayQueue() {
+		size = 0;
+		capacity = 2; 
+		arr = new Object[capacity];
+	}
+	
+	/**
+	 * returns size of the array
+	 */
 	@Override
 	public int size() {
 		return size;
 	}
 
+	/**
+	 * return true if array is empty
+	 */
 	@Override
 	public boolean isEmpty() {
 		return (size == 0);
 	}
 
+	/**
+	 * 
+	 */
+	@SuppressWarnings("unchecked")
 	@Override
 	public E front() throws EmptyQueueException {
 		if (isEmpty()) {
 			throw new EmptyQueueException();
 		}
 		else {
-			return elements[frontIndex];
+			return (E) arr[frontIndex];
 		}
 	}
 
+	/**
+	 * 
+	 */
 	@Override
-	public void enqueue(E e) throws FullQueueException {
+	public void enqueue(E e) {
 		// besser GROWABLE
 		if (size == capacity) {
-			throw new FullQueueException();
+			if (size == capacity) {
+				int newCapacity = capacity*2;
+				Object[] newArr = new Object[newCapacity];
+				capacity = capacity*2;
+				// copy arr to new array
+				for (int i=0; i<size; i++) {
+					newArr[i] = arr[i];
+				}
+				arr = newArr;
+				capacity = newCapacity;
+			}
+			
+			arr[size] = e;
+			size++;
 		}
 		else {
-			elements[(frontIndex + size) % capacity] = e;
+			arr[(frontIndex + size) % capacity] = e;
 			size++;
 		}
 	}
 
+	/**
+	 * 
+	 */
 	@Override
 	public E dequeue() throws EmptyQueueException {
 		if (isEmpty()) {
 			throw new EmptyQueueException();
 		}
 		else {
-			E e = elements[frontIndex];
-			frontIndex = (frontIndex + 1) % capacity;
-			size--;
-			return e;
+			try {
+				@SuppressWarnings("unchecked")
+				E e = (E) arr[frontIndex];
+				frontIndex = (frontIndex + 1) % capacity;
+				size--;
+				return e;
+			}
+			catch(ClassCastException e) {
+				System.err.println("Cast Error");
+				return null;
+			}
 		}
 	}
 
